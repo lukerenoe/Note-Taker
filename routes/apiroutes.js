47c55778c196ae4e33f2807a8491149b9bdb15f6
Luke Renoe <lukerenoe@gmail.com>
2022-06-07 16:02:37 -0600

    preparing for heroku

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -8,8 +8,15 @@ router.get('/notes', (req, res) => {
     res.json(dbjson)
 })
 
-
-
-
-
+router.post("/notes", (req, res) => {
+let notetemplate = {
+    title: req.body.title,
+    text: req.body.text,
+    id: Math.floor(Math.random()*100,000)
+}
+dbjson.push(notetemplate)
+fs.writeFileSync("./db/db.json", JSON.stringify(dbjson))
+// res.json sends req changes to front end to render on screen
+res.json(dbjson)
+})
 module.exports = router
