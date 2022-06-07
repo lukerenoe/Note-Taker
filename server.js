d39610bb13c84b8cfbc0d407914ef45b5bfb4313
Luke Renoe <lukerenoe@gmail.com>
2022-06-07 15:19:48 -0600

    routes

--- server.js
+++ server.js
@@ -1,31 +1,19 @@
 const express = require('express');
 const path = require('path');
-const { clog } = require('./middleware/clog');
-const api = require('./routes/index.js');
-
+// const api = require('./routes/index.js');
+const htmlroutes = require ('./routes/htmlroutes')
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-app.use(clog);
-
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
-
+// app.use('/api', api);
+app.use('/', htmlroutes)
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
-// GET Route for notes page
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/pages/notes.html'))
-);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
